06826d433666c91dc2d59c128c573590e3267e05
ksobo <k.sobolewski2@gmail.com>
2023-12-08 21:13:12 +0000

    Day1

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -1,23 +1,23 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int sum = 0;
-        BufferedReader myreader = new BufferedReader(new FileReader("Input.txt"));
-        while (myreader.readLine() != null) {
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("Input.txt");
+        BufferedReader myreader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = myreader.readLine();
+        while (line != null) {
             ArrayList<Integer> lastDigit = new ArrayList<>();
             ArrayList<Integer> firstDigit = new ArrayList<>();
             for (int i = 0; i <= 9; i++) {
-                lastDigit.add(myreader.readLine().lastIndexOf(Integer.toString(i)));
-                firstDigit.add(myreader.readLine().indexOf(Integer.toString(i)));
+                lastDigit.add(line.lastIndexOf(Integer.toString(i)));
+                firstDigit.add(line.indexOf(Integer.toString(i)));
             }
             int index1 = Collections.min(firstDigit);
             int index2 = Collections.max(lastDigit);
-            sum += Integer.parseInt(Integer.toString(index1) + Integer.toString(index2));
+            sum += Integer.parseInt(Integer.toString(index1) + index2);
         }
         System.out.println(sum);
     }
